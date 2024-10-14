7467ce9cf7e98641a56f0013f550654234778791
andresjf93 <129121808+andresjf93@users.noreply.github.com>
2024-10-14 15:47:42 -0500

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
+	metadataBase: new URL('https://tusitio.com'),
 	title: {
 		default: "Andresjf93",
 		template: "%s | Andresjf93",
