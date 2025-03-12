cf31eb228e1deead0c4c61cc6f666ec79a4579db
andresjf93 <129121808+andresjf93@users.noreply.github.com>
2025-03-12 12:37:24 -0500

    modificacion en project

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,7 +1,7 @@
 export default function ProjectsLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900 flex justify-center items-center">
-			<main className="w-full max-w-7xl p-4">{children}</main>
+		<div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900 ">
+			{children}
 		</div>
 	);
 }
